312cdbc9e5f50e1476876b795fe2f18ded8b8180
sts_user <sts_user@xfc.com>
2020-04-05 15:27:16 +0800

    增加了增删改查的测试

--- src/com/asu/entity/Test.java
+++ src/com/asu/entity/Test.java
@@ -1,6 +1,7 @@
 package com.asu.entity;
 
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -18,9 +19,48 @@ public class Test {
 			SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(is);
 			// 3.得到SqlSession对象
 			session = sf.openSession();
+
+			// 进行相应的操作(增)
+//			Student stu = new Student();
+//			stu.setSname("琴琴");
+//			stu.setSex("女");
+//			stu.setAge(9);
+//			int num = session.insert("addStu", stu);
+//			if (num > 0) {
+//				System.out.println("add success!");
+//			}
+
+			// 删
+//			int num = session.delete("deleteStu", 20);
+//			if (num > 0) {
+//				System.out.println("delete success!");
+//			}
+
+			// 改
+//			Student stu = new Student();
+//			stu.setSname("li琴");
+//			stu.setSno(9);
+//			int num = session.insert("updateStu", stu);
+//			if (num > 0) {
+//				System.out.println("update success!");
+//			}
+
 			// 查询单个学生
-			Student student = session.selectOne("selectStuBySno", 9);
-			System.out.println(student.getSname());
+//			Student student = session.selectOne("selectStuBySno", 9);
+//			System.out.println(student.getSname());
+
+			// 查询所有
+//			List<Student> sList = session.selectList("selectAllStu");
+//			for (Student student : sList) {
+//				System.out.println(student.getSname());
+//			}
+
+			// 按照名字模糊查询
+			List<Student> sList = session.selectList("selectBySname", "小");
+			for (Student stu : sList) {
+				System.out.println(stu.getSname());
+			}
+
 			// 提交事务
 			session.commit();
 		} catch (Exception e) {
